0b6015981f3754653b227baa80fb725626ff5766
apeleg <apeleg@outbrain.com>
2022-10-09 12:59:18 +0300

    add sockets

--- src/services/image.options.ts
+++
@@ -1,9 +0,0 @@
-export const imageOptions: imageOpt[] = [
-    {
-        name: 'paris.jpeg',
-        options: ['panama', 'chicago', 'jerusalem'],
-        answer: 'paris'
-    }
-]
-
-export type imageOpt  = {name: string, options: string[], answer: string}
