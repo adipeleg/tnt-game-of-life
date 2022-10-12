4b1360cf13491e803d2e8ab116a1191b0f4088fc
Shlomi Tamar <stamar@outbrain.com>
2022-10-12 16:14:24 +0300

    added questions

--- client/src/services/image.options.ts
+++ client/src/services/image.options.ts
@@ -1,14 +1,28 @@
 export const imageOptions: imageOpt[] = [
     {
+        category: 'Cities',
         name: 'paris.jpeg',
-        options: ['panama', 'chicago', 'jerusalem', 'paris'],
-        answer: 'paris'
+        options: ['Rome', 'Istanbul', 'Las vegas', 'Paris'],
+        answer: 'Paris'
     },
     {
-        name: 'paris.jpeg',
-        options: ['panama2', 'paris', 'chicago2', 'jerusalem'],
-        answer: 'paris'
+        category: 'Cities',
+        name: 'moscow.jpeg',
+        options: ['London', 'Moscow', 'Bangkok', 'Dubai'],
+        answer: 'Moscow'
+    },
+    {
+        category: 'Flags',
+        name: 'dk-flag.jpeg',
+        options: ['Denmark', 'Switzerland', 'Norway', 'England'],
+        answer: 'Denmark'
+    },
+    {
+        category: 'Actors',
+        name: 'leonardo.jpeg',
+        options: ['Michael J. Fox', 'Ben Affleck', 'Leonardo DiCaprio', 'Tobey Maguire'],
+        answer: 'Leonardo DiCaprio'
     }
 ]
 
-export type imageOpt = { name: string, options: string[], answer: string }
+export type imageOpt = { category: string, name: string, options: string[], answer: string }
